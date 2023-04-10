0131c7e32d19c5914361462b96faf53b951fdf8d
Anastasia <an.afalin@yandex.ru>
2023-04-10 14:52:09 +0300

    ver.2

--- static/custom.js
+++ static/custom.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
     // Попап
-    $(".cpp").click(function(event) {
+    $("#customers .item").click(function(event) {
         event.preventDefault();
-        let descrText = $(this).closest(".item").find(".descr").text();
+        let descrText = $(this).find(".descr").html();
         let pp = $(this).data('pp');
         $(".overlay .inner > *").hide();
         $(".overlay ."+pp).text(descrText);
@@ -12,5 +12,25 @@ $(document).ready(function() {
     $(".overlay").click(function(event) {
         if(!$(".popup").is(event.target) && $(".popup").has(event.target).length === 0 || event.target.className == "close") $(".overlay").fadeOut(150);
     });
+
+    // Маска телефона
+    $("input[name='PHONE']").mask("+7(999) 999-99-99").on('click', function (e) {
+        var caret = e.target.selectionStart;
+        var text = $(this).val();
+        var lastChar = text.indexOf('_');
+        if (lastChar < caret && lastChar !== -1) {
+            $(this).caret(lastChar);
+        }
+    }).on('keydown', function (e) {
+        if (e.keyCode === 39 || e.keyCode === 37) {
+            var caret = e.target.selectionStart;
+            var text = $(this).val();
+            var lastChar = text.indexOf('_');
+            if (lastChar <= caret && lastChar !== -1) {
+                $(this).caret(lastChar - 1);
+            }
+        }
+    });
+
 
 });
